b62a59e79b48043d9ede07aed22c179bfa164f1e
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-06-10 14:40:41 +0200

    persons

--- src/main/scala/u02/Modules.scala
+++ src/main/scala/u02/Modules.scala
@@ -1,5 +1,9 @@
 package u02
 
+import u03.Sequences.*
+import u03.Sequences.Sequence.*
+
+
 object Modules extends App:
 
   // An ADT: type + module
@@ -12,6 +16,10 @@ object Modules extends App:
       case Student(n, _) => n
       case Teacher(n, _) => n
 
+    def course(p: Person): String = p match
+      case Teacher(_, c) => c
+      case _ => ""
+
   println(Person.name(Person.Student("mario", 2015)))
 
   import Person.*
@@ -22,6 +30,16 @@ object Modules extends App:
   def isStudent(p: Person): Boolean = p match
     case Student(_, _) => true
     case _ => false
+
+  def courses(persons: Sequence[Person]): Sequence[String] = map(filter(persons)(!isStudent(_)))(course)
+
+  def foldLeft[A,B](s: Sequence[A])(acc: B)(op: (B, A) => B): B = s match
+    case Cons(h, t) => foldLeft(t)(op(acc, h))(op)
+    case _ => acc
+
+  def countCourses(persons: Sequence[Person]): Int =
+    foldLeft(map(courses(persons))(_ => 1))(0)(_ + _)
+
 
   println(isStudent(Student("mario", 2015)))
 end Modules
